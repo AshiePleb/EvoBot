e99ffa9afbc31d2e3224377ca4311f91eb08ffe9
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-10-23 17:21:28 +0100

    Update EVO.py

--- EVO.py
+++ EVO.py
@@ -51,6 +51,7 @@ async def on_message(message):
       embed.add_field(name="<:EvoDiamond:504246347298242573> bye <:EvoDiamond:504246347298242573>", value="<:EvoBlob1:504249845368750082> Triggers when you say bye <:EvoBlob1:504249845368750082>", inline=False)
       embed.add_field(name="<:EvoDiamond:504246347298242573> say <:EvoDiamond:504246347298242573>", value="<:EvoBlob1:504249845368750082> Makes the bot say something when you use %say msg <:EvoBlob1:504249845368750082>", inline=False)
       embed.add_field(name="<:EvoDiamond:504246347298242573> info <:EvoDiamond:504246347298242573>", value="<:EvoBlob1:504249845368750082> Lists the EVO Servers infomation <:EvoBlob1:504249845368750082>", inline=False) 
+      embed.add_field(name="<:EvoDiamond:504246347298242573> staff <:EvoDiamond:504246347298242573>", value="<:EvoBlob1:504249845368750082> Lists EVO Staff Team <:EvoBlob1:504249845368750082>", inline=False)
       await client.send_message(message.channel, embed=embed) 
       
     if message.content.startswith('%staff'):
