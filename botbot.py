268e7795aaf0684551b092317dc17e156d65d762
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-10-23 14:30:22 +0100

    Create botbot.py

---
+++ botbot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import os
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "=")
+@client.event
+async def on_ready():
+    print("BobtBot Online!")
+    await client.change_presence(game=discord.Game(name="BotBot"))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('=hello'):
+        msg = 'Hello {0.author.mention} How Are You Today'.format(message)
+        await client.send_message(message.channel, msg)
+        
+    if message.content.startswith('=bye'):
+        msg = 'GoodBye {0.author.mention} Hope To See You Again Soon :wave: '.format(message)
+        await client.send_message(message.channel, msg)
+
+   
+
+client.run(os.getenv('TOKEN'))
