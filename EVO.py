27ca294d2c5853e27d5693c46b1ad71b6deb3b56
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-10-23 16:34:12 +0100

    Update EVO.py

--- EVO.py
+++ EVO.py
@@ -6,14 +6,14 @@ import time
 import os
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "=")
+client = commands.Bot(command_prefix = "%")
 client.remove_command("help")
 @client.event
 async def on_ready():
     print("Bot is ready!")
     await client.change_presence(game=discord.Game(name="EVO <3"))
     await asyncio.sleep(10)
-    await client.change_presence(game=discord.Game(name="Poly <3"))
+    await client.change_presence(game=discord.Game(name="PolyBalls <3"))
     await asyncio.sleep(10)
     await client.change_presence(game=discord.Game(name="Jack <3"))
     await asyncio.sleep(10)
@@ -22,29 +22,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('=hello'):
+    if message.content.startswith('hello'):
         msg = 'Hello {0.author.mention}, How Are You Today? <:EvoBlob1:504249845368750082>'.format(message)
         await client.send_message(message.channel, msg)
         
-    if message.content.startswith('=bye'):
+    if message.content.startswith('bye'):
         msg = 'GoodBye {0.author.mention}, Hope To See You Again Soon <:EvoBlob1:504249845368750082>'.format(message)
         await client.send_message(message.channel, msg)
-
-    if message.content.startswith('=ip'):
-        if message.author.id == "475825699123494932":
-            msg = '{0.author.mention} The Server IP Is [IP]'.format(message)
-            await client.send_message(message.channel, msg)
-        else: 
-            await client.send_message(message.channel, "Only AntiSepticBoi#9970 Can Run This Command!")
             
-    if message.content.startswith('=invite'):
+    if message.content.startswith('%invite'):
         if message.author.id == "475825699123494932":
             msg = 'https://discordapp.com/api/oauth2/authorize?client_id=499643998194106369&permissions=0&scope=bot'.format(message)
             await client.send_message(message.channel, msg)
         else: 
             await client.send_message(message.channel, "Only AntiSepticBoi#9970 Can Run This Command!")            
    
-    if message.content.startswith('/infomation'):
+    if message.content.startswith('%info','%infomation'):
       embed = discord.Embed(title="EVO Server Infomation", color=0xff00ff)
       embed.add_field(name="<:EvoDiamond:504246347298242573> IP <:EvoDiamond:504246347298242573>", value="<:EvoBlob1:504249845368750082> Coming Soon! <:EvoBlob1:504249845368750082>", inline=False)
       embed.add_field(name="<:EvoDiamond:504246347298242573> Forum <:EvoDiamond:504246347298242573>", value="<:EvoBlob1:504249845368750082> http://evonetwork.ga/ <:EvoBlob1:504249845368750082>", inline=False)
