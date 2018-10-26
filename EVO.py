eb1cd94cb134a4a30d05b8a5b325a216a4d2b6ca
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-10-26 20:05:37 +0100

    Update EVO.py

--- EVO.py
+++ EVO.py
@@ -46,7 +46,7 @@ async def on_message(message):
 @client.command(pass_context=True)
 async def night(ctx):
        await client.say("***{}*** Has gone to bed goodnight! :zzz:".format(ctx.message.author))
-       await client.add_reaction(ctx.message, "zzz")
+       await client.add_reaction(ctx.message, "505440307085836288")
        await client.delete_message(ctx.message)
 @client.command(pass_context=True)
 async def info(ctx):
