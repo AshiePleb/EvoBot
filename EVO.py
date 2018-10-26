0a1472170a8a35c9d89e257d7fe3701c15fa5f38
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-10-26 20:46:37 +0100

    Update EVO.py

--- EVO.py
+++ EVO.py
@@ -45,9 +45,8 @@ async def on_message(message):
         await client.process_commands(message)
 @client.command(pass_context=True)
 async def night(ctx):
-       await client.delete_message(ctx.message)
-       await client.say("**{}** Has gone to bed night! :zzz:".format(ctx.message.author))
-       emoji = get(client.get_all_emojis(),id='505466595267837962')
+       await client.say("{} has gone to bed :zzz:".format(ctx.message.author))
+       emoji = get(client.get_all_emojis(),id='505440307085836288')
        await client.add_reaction(ctx.message, emoji)
 @client.command(pass_context=True)
 async def info(ctx):
