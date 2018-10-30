67f1655199d2f2d23402513c2a909733501db7f8
Jackaboi18 <38188828+Jackaboi18@users.noreply.github.com>
2018-10-30 13:31:34 +0000

    Update EVO.py

--- EVO.py
+++ EVO.py
@@ -27,7 +27,18 @@ async def change_status():
 @client.event
 async def on_ready():
     print("bot is ready!")
-    
+
+@client.event
+async def on_message(message):
+    contents = message.content.split(" ")
+    for word in contents:
+        if word.upper() in chat_filter:
+            if not message.author.id in bypass_list:
+                try:
+                    await client.delete_message(message)
+                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
+                except discord.errors.NotFound:
+                    return    
 @client.event
 async def on_message(message):
     if message.content.startswith('hello'):
@@ -55,17 +66,6 @@ async def on_message(message):
             await client.process_commands(message)
     else:
         await client.process_commands(message)
-@client.event
-async def on_message(message):
-    contents = message.content.split(" ")
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
-                except discord.errors.NotFound:
-                    return
         
 @client.command(pass_context=True)
 async def night(ctx):
